ce06f87b7a594543af301d78e8f13aa88b54b716
Isaac Nabil <nabilkhafali@gmail.com>
2020-09-12 10:32:45 +0000

    added logo

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -5,7 +5,7 @@
  <header id="header" class="header">
     <div class="top-left">
         <div class="navbar-header">
-            <a class="navbar-brand" href="{{ route('dashboard') }}"><img src="{{ asset('assets/images/logo.png') }}" alt="Logo"></a>
+            <a class="navbar-brand" href="{{ route('dashboard') }}"><img src="{{ asset('assets/images/danlogo.png') }}" alt="Logo"></a>
             <a class="navbar-brand hidden" href="{{ route('dashboard') }}"><img src="{{ asset('assets/images/logo2.png') }}" alt="Logo"></a>
             <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
         </div>
